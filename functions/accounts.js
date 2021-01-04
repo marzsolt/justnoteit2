7cad743c4eeecf4d3e8c7092374eda00bb0c03b0
marzsolt <markos.zsolt.25@gmail.com>
2021-01-04 18:36:47 +0100

    try sth

--- functions/accounts.js
+++ functions/accounts.js
@@ -1,12 +1,17 @@
 exports.handler = async function(event, context) {
-    console.log("Event:");
+    /*console.log("Event:");
     console.log(event);
 
     console.log("Context:");
-    console.log(context);
+    console.log(context);*/
+    
+    const { accessToken } = event.body;
+    console.log(accessToken);
+
+    //await fetch(`https://graph.facebook.com/v8.0/me?access_token=${accessToken}`)
 
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello fucking World"})
+        body: JSON.stringify({message: "Hello fucking World again"})
     };
 }
