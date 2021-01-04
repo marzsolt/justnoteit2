52491897a852f7983787c823ee6dfe6968e331a8
marzsolt <markos.zsolt.25@gmail.com>
2021-01-04 17:10:09 +0100

    try functions

--- functions/accounts.js
+++ functions/accounts.js
@@ -1,6 +1,23 @@
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
-}
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+/*function updateDatabase(data) {
+  ... // update the database
+  return newValue;
+}*/
+
+app.use(bodyParser);
+app.post('/authenticate', (req, res) => {
+    //const newValue = updateDatabase(res.body);
+    
+    const { accessToken } = req.body;
+
+    res.json({
+        token: accessToken
+    });
+});
+
+module.exports.handler = serverless(app);
